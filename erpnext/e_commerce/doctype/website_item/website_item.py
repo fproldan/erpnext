e4c8f64cb276840fe24f8d7db0df78bf2b9e57f3
marination <maricadsouza221197@gmail.com>
2021-09-30 18:34:26 +0530

    fix: Website Items with same Item name unhandled, thumbnails missing
    - Use naming series for Website Item. There could be two items with same name and different item code
    - Fix: Website Item Page view breaks if cart is disabled
    - Fix: thumbnails not created for Website Items after patch
    - Fix: ‘Request for Quote’ button & cart summary not visible if checkout is disabled
    
    (cherry picked from commit 36b519c962920ef08d693639077648df787a4630)

--- erpnext/e_commerce/doctype/website_item/website_item.py
+++ erpnext/e_commerce/doctype/website_item/website_item.py
@@ -36,7 +36,6 @@ class WebsiteItem(WebsiteGenerator):
 	def autoname(self):
 		# use naming series to accomodate items with same name (different item code)
 		from frappe.model.naming import make_autoname
-
 		from erpnext.setup.doctype.naming_series.naming_series import get_default_naming_series
 
 		naming_series = get_default_naming_series("Website Item")
