07718b9280f15e343488f2ef4dc4986d9b65c02f
Francisco Roldan <franciscoproldan@gmail.com>
2021-10-14 12:27:42 -0300

    fix: cashier

--- erpnext/accounts/doctype/apertura_de_caja/apertura_de_caja.js
+++ erpnext/accounts/doctype/apertura_de_caja/apertura_de_caja.js
@@ -8,12 +8,7 @@ frappe.ui.form.on('Apertura de Caja', {
 			frm.set_value('period_start_date', frappe.datetime.now_datetime());
 			frm.set_value('user', frappe.session.user);
 			frm.refresh_field("user");
-
-			if (!has_admin_perms()) {
-				frm.set_df_property('user', 'hidden', 1);
-				frm.set_df_property('user', 'read_only', 1);
-				frm.refresh_field("user");
-			}
+			show_hide_user(frm);
 		}
 
 		frm.set_query("user", function(doc) {
@@ -25,7 +20,9 @@ frappe.ui.form.on('Apertura de Caja', {
 			};
 		});
 	},
-
+	onload(frm) {
+		if (frm.doc.docstatus == 0) {show_hide_user(frm);}
+	},
 	refresh(frm) {
 		// set default posting date / time
 		if(frm.doc.docstatus == 0) {
@@ -33,9 +30,9 @@ frappe.ui.form.on('Apertura de Caja', {
 				frm.set_value('posting_date', frappe.datetime.nowdate());
 			}
 			frm.trigger('set_posting_date_read_only');
+			show_hide_user(frm);
 		}
 	},
-
 	set_posting_date_read_only(frm) {
 		if(frm.doc.docstatus == 0 && frm.doc.set_posting_date) {
 			frm.set_df_property('posting_date', 'read_only', 0);
@@ -43,14 +40,25 @@ frappe.ui.form.on('Apertura de Caja', {
 			frm.set_df_property('posting_date', 'read_only', 1);
 		}
 	},
-
 	set_posting_date(frm) {
 		frm.trigger('set_posting_date_read_only');
 	},
-	
 });
 
 
 function has_admin_perms(frm) {
 	return frappe.user.has_role('System Manager') || frappe.user.has_role('Accounts Manager') || frappe.user.has_role('Administrator');
+}
+
+function show_hide_user(frm) {
+	if (!frm.doc.user) {
+		frm.set_value('user', frappe.session.user);
+		frm.refresh_field("user");
+	}
+
+	if (!has_admin_perms()) {
+		frm.set_df_property('user', 'hidden', 1);
+		frm.set_df_property('user', 'read_only', 1);
+		frm.refresh_field("user");
+	}
 }
