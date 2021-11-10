907d364207b14079e406d7134b01e84009ee3bf7
Francisco Roldan <franciscoproldan@gmail.com>
2021-11-10 11:51:56 -0300

    feat: set roles on domain setup

--- erpnext/support/setup.py
+++ erpnext/support/setup.py
@@ -8,12 +8,13 @@ def setup_support():
 
 
 def add_custom_roles_for_reports():
-    if not frappe.db.get_value('Custom Role', dict(report='Support Hour Distribution')):
-        frappe.get_doc(dict(
-            doctype='Custom Role',
-            report='Support Hour Distribution',
-            roles= [
-                dict(role='Support Team'),
-                dict(role='System Manager')
-            ]
-        )).insert()
+    for report in ['Support Hour Distribution', 'Issue Summary', 'Issue Analytics', 'First Response Time for Issues']:
+        if not frappe.db.get_value('Custom Role', dict(report=report)):
+            frappe.get_doc(dict(
+                doctype='Custom Role',
+                report=report,
+                roles=[
+                    dict(role='Support Team'),
+                    dict(role='System Manager')
+                ]
+            )).insert()
