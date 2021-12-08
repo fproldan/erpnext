faea835fc0bd0c9552f776662a5643cbe6db6278
Francisco Roldan <franciscoproldan@gmail.com>
2021-12-08 17:30:36 -0300

    feat: website

---
+++ erpnext/patches/v13_0/delete_web_forms.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2019, Frappe and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+
+import frappe
+
+
+def execute():
+    for name in ["student-applicant", "lab-test", "patient-registration", "prescription", "patient-appointments", "personal-details", "certification-application-usd", "certification-application", "grant-application"]:
+        frappe.db.delete("Web Form", name)
+    frappe.db.commit()
