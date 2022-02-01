1e02563bd2e79451f1d5045e1ea4cac22478c446
Francisco Roldán <franciscoproldan@gmail.com>
2022-02-01 12:20:25 -0300

    Update delete_gst_payment_entry_fields.py

--- erpnext/patches/v13_0/delete_gst_payment_entry_fields.py
+++ erpnext/patches/v13_0/delete_gst_payment_entry_fields.py
@@ -8,6 +8,7 @@ def execute():
     field = frappe.db.get_value("Custom Field", {"dt": "Payment Entry", "fieldname": 'gst_section'})
     if field:
         custom_field = frappe.get_doc("Custom Field", field)
+		custom_field.flags.ignore_validate = True
         custom_field.hidden = 1
         custom_field.save()
         frappe.clear_cache(doctype="Payment Entry")
