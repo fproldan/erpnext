d1df5caf6581ff6bb871beac5a9f62cf65fae9dd
Francisco Roldan <franciscoproldan@gmail.com>
2022-02-18 11:33:12 -0300

    fix: after migrate

---
+++ erpnext/patches/v13_0/remove_report.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+import frappe
+import erpnext.hooks as hooks
+
+
+def execute():
+    hooks.after_migrate.append("erpnext.patches.v13_0.remove_report.delete_report")
+
+
+def delete_report():
+    frappe.delete_doc('Report', 'Sales Payment Summary', ignore_missing=True)
