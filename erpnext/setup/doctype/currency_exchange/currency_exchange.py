378c9bfc649578a6d1bbd01dbc47314e71b9fca1
Lucas Vazquez <lucas5zvazquez@gmail.com>
2022-03-16 10:12:33 -0300

    Obtener el precio del dolar diariamente (#94)
    * feat: get exchange rates daily
    
    * improve: format code
    
    * fix: inverse convertion
    
    * feat: allow to select the exchange rate type
    
    * fix: change logic
    
    * improve: improve code

--- erpnext/setup/doctype/currency_exchange/currency_exchange.py
+++ erpnext/setup/doctype/currency_exchange/currency_exchange.py
@@ -5,7 +5,12 @@
 
 from __future__ import unicode_literals
 
-from frappe import _, throw
+import itertools
+from typing import Optional
+
+import requests
+from frappe import _, db, get_all, new_doc, throw
+from frappe.exceptions import DuplicateEntryError
 from frappe.model.document import Document
 from frappe.utils import cint, formatdate, get_datetime_str, nowdate
 
@@ -34,3 +39,41 @@ class CurrencyExchange(Document):
 
 		if not cint(self.for_buying) and not cint(self.for_selling):
 			throw(_("Currency Exchange must be applicable for Buying or for Selling."))
+
+
+def get_currency_exchange_rate(from_currency: str, to_currency: str) -> Optional[float]:
+	exchange_rate_type = db.get_value(
+		"Currency",
+		{"currency_name": to_currency if from_currency == "ARS" else from_currency},
+		"exchange_rate_type",
+	)
+	return next(
+		(
+			1 / float(data["casa"]["venta"].replace(",", "."))
+			if from_currency == "ARS"
+			else float(data["casa"]["compra"].replace(",", "."))
+			for data in requests.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales").json()
+			if data["casa"]["nombre"] == exchange_rate_type
+		),
+		None,
+	)
+
+
+def set_currency_exchange_rates(*args, **kwargs):
+	for currency_pair in tuple(
+		itertools.product(
+			("ARS",), get_all("Currency", {"exchange_rate_type": ("!=", "")}, pluck="name")
+		)
+	):
+		for from_currency, to_currency in (currency_pair, currency_pair[::-1]):
+			exchange_rate = get_currency_exchange_rate(from_currency, to_currency)
+			if exchange_rate is None:
+				continue
+			currency_exchange = new_doc("Currency Exchange")
+			currency_exchange.from_currency = from_currency
+			currency_exchange.to_currency = to_currency
+			currency_exchange.exchange_rate = exchange_rate
+			try:
+				currency_exchange.insert()
+			except DuplicateEntryError:
+				pass
