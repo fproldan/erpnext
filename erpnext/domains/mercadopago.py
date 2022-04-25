73a03b0db4622dd7fb1e70674683e4fd8bed0b87
Francisco Roldán <franciscoproldan@gmail.com>
2022-04-25 10:36:18 -0300

    feat: Mercadopago domain (#125) (#142)

---
+++ erpnext/domains/mercadopago.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+data = {
+    'modules': [
+        'Mercadopago'
+    ],
+}
--- erpnext/setup/setup_wizard/operations/install_fixtures.py
+++ erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -77,7 +77,8 @@ def install(country=None):
 		{'doctype': 'Domain', 'domain': 'Pagos360'},
 		{'doctype': 'Domain', 'domain': 'Mercadolibre'},
 		{'doctype': 'Domain', 'domain': 'Shopify'},
-
+		{'doctype': 'Domain', 'domain': 'Mercadopago'},
+		
 		# ensure at least an empty Address Template exists for this Country
 		{'doctype':"Address Template", "country": country},
 
