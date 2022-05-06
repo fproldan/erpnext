67134b8871141ac201dca33da3430f37cf18daea
Francisco Roldan <franciscoproldan@gmail.com>
2022-05-06 13:38:29 -0300

    fix: hide tools button

--- erpnext/buying/report/supplier_quotation_comparison/supplier_quotation_comparison.js
+++ erpnext/buying/report/supplier_quotation_comparison/supplier_quotation_comparison.js
@@ -111,72 +111,72 @@ frappe.query_reports["Supplier Quotation Comparison"] = {
 		return value;
 	},
 
-	onload: (report) => {
-		// Create a button for setting the default supplier
-		report.page.add_inner_button(__("Select Default Supplier"), () => {
-			let reporter = frappe.query_reports["Supplier Quotation Comparison"];
+	// onload: (report) => {
+	// 	// Create a button for setting the default supplier
+	// 	report.page.add_inner_button(__("Select Default Supplier"), () => {
+	// 		let reporter = frappe.query_reports["Supplier Quotation Comparison"];
 
-			//Always make a new one so that the latest values get updated
-			reporter.make_default_supplier_dialog(report);
-		}, __("Tools"));
+	// 		//Always make a new one so that the latest values get updated
+	// 		reporter.make_default_supplier_dialog(report);
+	// 	}, __("Tools"));
 
-	},
-	make_default_supplier_dialog: (report) => {
-		// Get the name of the item to change
-		if(!report.data) return;
+	// },
+	// make_default_supplier_dialog: (report) => {
+	// 	// Get the name of the item to change
+	// 	if(!report.data) return;
 
-		let filters = report.get_values();
-		let item_code = filters.item_code;
+	// 	let filters = report.get_values();
+	// 	let item_code = filters.item_code;
 
-		if (!item_code) {
-			frappe.msgprint("Debe seleccionar un producto en el filtro de productos");
-			return;
-		};
+	// 	if (!item_code) {
+	// 		frappe.msgprint("Debe seleccionar un producto en el filtro de productos");
+	// 		return;
+	// 	};
 
-		// Get a list of the suppliers (with a blank as well) for the user to select
-		let suppliers = $.map(report.data, (row, idx)=>{ return row.supplier_name })
+	// 	// Get a list of the suppliers (with a blank as well) for the user to select
+	// 	let suppliers = $.map(report.data, (row, idx)=>{ return row.supplier_name })
 
-		// Create a dialog window for the user to pick their supplier
-		let dialog = new frappe.ui.Dialog({
-			title: __('Select Default Supplier'),
-			fields: [
-				{
-					reqd: 1,
-					label: 'Supplier',
-					fieldtype: 'Link',
-					options: 'Supplier',
-					fieldname: 'supplier',
-					get_query: () => {
-						return {
-							filters: {
-								'name': ['in', suppliers]
-							}
-						}
-					}
-				}
-			]
-		});
+	// 	// Create a dialog window for the user to pick their supplier
+	// 	let dialog = new frappe.ui.Dialog({
+	// 		title: __('Select Default Supplier'),
+	// 		fields: [
+	// 			{
+	// 				reqd: 1,
+	// 				label: 'Supplier',
+	// 				fieldtype: 'Link',
+	// 				options: 'Supplier',
+	// 				fieldname: 'supplier',
+	// 				get_query: () => {
+	// 					return {
+	// 						filters: {
+	// 							'name': ['in', suppliers]
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		]
+	// 	});
 
-		dialog.set_primary_action(__("Set Default Supplier"), () => {
-			let values = dialog.get_values();
-			if(values) {
-				// Set the default_supplier field of the appropriate Item to the selected supplier
-				frappe.call({
-					method: "frappe.client.set_value",
-					args: {
-						doctype: "Item",
-						name: item_code,
-						fieldname: "default_supplier",
-						value: values.supplier,
-					},
-					freeze: true,
-					callback: (r) => {
-						frappe.msgprint(__("Successfully Set Supplier"));
-						dialog.hide();
-					}
-				});
-			}
-		});
-		dialog.show();
-	}
+	// 	dialog.set_primary_action(__("Set Default Supplier"), () => {
+	// 		let values = dialog.get_values();
+	// 		if(values) {
+	// 			// Set the default_supplier field of the appropriate Item to the selected supplier
+	// 			frappe.call({
+	// 				method: "frappe.client.set_value",
+	// 				args: {
+	// 					doctype: "Item",
+	// 					name: item_code,
+	// 					fieldname: "default_supplier",
+	// 					value: values.supplier,
+	// 				},
+	// 				freeze: true,
+	// 				callback: (r) => {
+	// 					frappe.msgprint(__("Successfully Set Supplier"));
+	// 					dialog.hide();
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// 	dialog.show();
+	// }
 }
