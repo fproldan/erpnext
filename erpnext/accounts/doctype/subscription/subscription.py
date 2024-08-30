da0600f15170b8057e2d0cb6028a09f1179b8984
fproldan <franciscoproldan@gmail.com>
2024-08-30 14:30:20 +0000

    fix: persat disabled

--- erpnext/accounts/doctype/subscription/subscription.py
+++ erpnext/accounts/doctype/subscription/subscription.py
@@ -368,12 +368,12 @@ class Subscription(Document):
 			invoice.territory = frappe.db.get_value('Customer', self.party, 'territory')
 
 			if self.punto_de_venta and self.secuencia:
-				invoice.naming_series = self.secuencia
 				punto_de_venta = frappe.get_doc('Punto de Venta', self.punto_de_venta)
 				tipo_de_comprobante = punto_de_venta.get_tipo_comprobante_for_secuence(self.secuencia)
 				tipo_comprobante_afip = tipo_de_comprobante.codigo
 				if tipo_comprobante_afip in ['201', '206', '211']:  # MIPYME
 					subscription_settings = frappe.get_single('Subscription Settings')
+					invoice.naming_series = self.secuencia
 					invoice.opcion_de_transmision = subscription_settings.opcion_de_transmision
 		else:
 			invoice.supplier = self.party
