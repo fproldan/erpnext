7971b90a0b6b7c0805778296e3cb8591142ebc3c
Francisco Roldán <franciscoproldan@gmail.com>
2024-09-11 12:17:59 -0300

    Comisiones de Pago (#865)
    * feat: comisiones en pago
    
    * feat: backend
    
    * feat: commission
    
    * feat
    : report
    
    * feat: report
    
    * feat: autofetch

--- erpnext/selling/report/sales_person_wise_transaction_summary/sales_person_wise_transaction_summary.py
+++ erpnext/selling/report/sales_person_wise_transaction_summary/sales_person_wise_transaction_summary.py
@@ -21,11 +21,17 @@ def execute(filters=None):
 
 	for d in entries:
 		if d.stock_qty > 0 or filters.get('show_return_entries', 0):
-			data.append([
-				d.name, d.customer, d.territory, d.warehouse, d.posting_date, d.item_code,
-				item_details.get(d.item_code, {}).get("item_group"), item_details.get(d.item_code, {}).get("brand"),
-				d.stock_qty, d.base_net_amount, d.sales_person, d.allocated_percentage, d.contribution_amt, company_currency
-			])
+			if filters.get("doc_type") == "Payment Entry":
+				data.append([
+					d.name, d.customer, d.posting_date,
+					d.base_net_amount, d.sales_person, d.allocated_percentage, d.contribution_amt, company_currency
+				])
+			else:
+				data.append([
+					d.name, d.customer, d.territory, d.warehouse, d.posting_date, d.item_code,
+					item_details.get(d.item_code, {}).get("item_group"), item_details.get(d.item_code, {}).get("brand"),
+					d.stock_qty, d.base_net_amount, d.sales_person, d.allocated_percentage, d.contribution_amt, company_currency
+				])
 
 	if data:
 		total_row = [""]*len(data[0])
@@ -37,103 +43,161 @@ def get_columns(filters):
 	if not filters.get("doc_type"):
 		msgprint(_("Please select the document type first"), raise_exception=1)
 
-	columns = [
-		{
-			"label": _(filters["doc_type"]),
-			"options": filters["doc_type"],
-			"fieldname": frappe.scrub(filters['doc_type']),
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Customer"),
-			"options": "Customer",
-			"fieldname": "customer",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Territory"),
-			"options": "Territory",
-			"fieldname": "territory",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Warehouse"),
-			"options": "Warehouse",
-			"fieldname": "warehouse",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Posting Date"),
-			"fieldname": "posting_date",
-			"fieldtype": "Date",
-			"width": 140
-		},
-		{
-			"label": _("Item Code"),
-			"options": "Item",
-			"fieldname": "item_code",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Item Group"),
-			"options": "Item Group",
-			"fieldname": "item_group",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Brand"),
-			"options": "Brand",
-			"fieldname": "brand",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Qty"),
-			"fieldname": "qty",
-			"fieldtype": "Float",
-			"width": 140
-		},
-		{
-			"label": _("Amount"),
-			"options": "currency",
-			"fieldname": "amount",
-			"fieldtype": "Currency",
-			"width": 140
-		},
-		{
-			"label": _("Sales Person"),
-			"options": "Sales Person",
-			"fieldname": "sales_person",
-			"fieldtype": "Link",
-			"width": 140
-		},
-		{
-			"label": _("Contribution %"),
-			"fieldname": "contribution",
-			"fieldtype": "Float",
-			"width": 140
-		},
-		{
-			"label": _("Contribution Amount"),
-			"options": "currency",
-			"fieldname": "contribution_amt",
-			"fieldtype": "Currency",
-			"width": 140
-		},
-		{
-			"label":_("Currency"),
-			"options": "Currency",
-			"fieldname":"currency",
-			"fieldtype":"Link",
-			"hidden" : 1
-		}
-	]
+	if filters.get("doc_type") == "Payment Entry":
+			columns = [
+			{
+				"label": _(filters["doc_type"]),
+				"options": filters["doc_type"],
+				"fieldname": frappe.scrub(filters['doc_type']),
+				"fieldtype": "Link",
+				"width": 180
+			},
+			{
+				"label": _("Customer"),
+				"options": "Customer",
+				"fieldname": "customer",
+				"fieldtype": "Link",
+				"width": 180
+			},
+			{
+				"label": _("Posting Date"),
+				"fieldname": "posting_date",
+				"fieldtype": "Date",
+				"width": 200
+			},
+			{
+				"label": _("Amount"),
+				"options": "currency",
+				"fieldname": "amount",
+				"fieldtype": "Currency",
+				"width": 120
+			},
+			{
+				"label": _("Sales Person"),
+				"options": "Sales Person",
+				"fieldname": "sales_person",
+				"fieldtype": "Link",
+				"width": 180
+			},
+			{
+				"label": _("Contribution %"),
+				"fieldname": "contribution",
+				"fieldtype": "Float",
+				"width": 140
+			},
+			{
+				"label": _("Contribution Amount"),
+				"options": "currency",
+				"fieldname": "contribution_amt",
+				"fieldtype": "Currency",
+				"width": 180
+			},
+			{
+				"label":_("Currency"),
+				"options": "Currency",
+				"fieldname":"currency",
+				"fieldtype":"Link",
+				"hidden" : 1
+			}
+		]
+	else:
+		columns = [
+			{
+				"label": _(filters["doc_type"]),
+				"options": filters["doc_type"],
+				"fieldname": frappe.scrub(filters['doc_type']),
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Customer"),
+				"options": "Customer",
+				"fieldname": "customer",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Territory"),
+				"options": "Territory",
+				"fieldname": "territory",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Warehouse"),
+				"options": "Warehouse",
+				"fieldname": "warehouse",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Posting Date"),
+				"fieldname": "posting_date",
+				"fieldtype": "Date",
+				"width": 140
+			},
+			{
+				"label": _("Item Code"),
+				"options": "Item",
+				"fieldname": "item_code",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Item Group"),
+				"options": "Item Group",
+				"fieldname": "item_group",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Brand"),
+				"options": "Brand",
+				"fieldname": "brand",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Qty"),
+				"fieldname": "qty",
+				"fieldtype": "Float",
+				"width": 140
+			},
+			{
+				"label": _("Amount"),
+				"options": "currency",
+				"fieldname": "amount",
+				"fieldtype": "Currency",
+				"width": 140
+			},
+			{
+				"label": _("Sales Person"),
+				"options": "Sales Person",
+				"fieldname": "sales_person",
+				"fieldtype": "Link",
+				"width": 140
+			},
+			{
+				"label": _("Contribution %"),
+				"fieldname": "contribution",
+				"fieldtype": "Float",
+				"width": 140
+			},
+			{
+				"label": _("Contribution Amount"),
+				"options": "currency",
+				"fieldname": "contribution_amt",
+				"fieldtype": "Currency",
+				"width": 140
+			},
+			{
+				"label":_("Currency"),
+				"options": "Currency",
+				"fieldname":"currency",
+				"fieldtype":"Link",
+				"hidden" : 1
+			}
+		]
 
 	return columns
 
@@ -145,29 +209,45 @@ def get_entries(filters):
 		qty_field = "qty"
 	conditions, values = get_conditions(filters, date_field)
 
-	entries = frappe.db.sql("""
-		SELECT
-			dt.name, dt.customer, dt.territory, dt.%s as posting_date, dt_item.item_code,
-			st.sales_person, st.allocated_percentage, dt_item.warehouse,
-		CASE
-			WHEN dt.status = "Closed" THEN dt_item.%s * dt_item.conversion_factor
-			ELSE dt_item.stock_qty
-		END as stock_qty,
-		CASE
-			WHEN dt.status = "Closed" THEN (dt_item.base_net_rate * dt_item.%s * dt_item.conversion_factor)
-			ELSE dt_item.base_net_amount
-		END as base_net_amount,
-		CASE
-			WHEN dt.status = "Closed" THEN ((dt_item.base_net_rate * dt_item.%s * dt_item.conversion_factor) * st.allocated_percentage/100)
-			ELSE dt_item.base_net_amount * st.allocated_percentage/100
-		END as contribution_amt
-		FROM
-			`tab%s` dt, `tab%s Item` dt_item, `tabSales Team` st
-		WHERE
-			st.parent = dt.name and dt.name = dt_item.parent and st.parenttype = %s
-			and dt.docstatus = 1 %s order by st.sales_person, dt.name desc
-		""" %(date_field, qty_field, qty_field, qty_field, filters["doc_type"], filters["doc_type"], '%s', conditions),
-			tuple([filters["doc_type"]] + values), as_dict=1)
+	if filters["doc_type"] == "Payment Entry":
+			entries = frappe.db.sql("""
+			SELECT
+				dt.name, dt.party as customer, dt.%s as posting_date,
+				st.sales_person, st.allocated_percentage,
+			1 as stock_qty,
+			dt.paid_amount as base_net_amount,
+			dt.paid_amount * st.allocated_percentage/100 as  contribution_amt
+			FROM
+				`tab%s` dt, `tab%s Reference` dt_item, `tabSales Team` st
+			WHERE
+				st.parent = dt.name and dt.name = dt_item.parent and st.parenttype = %s
+				and dt.docstatus = 1 %s order by st.sales_person, dt.name desc
+			""" %(date_field, filters["doc_type"], filters["doc_type"], '%s', conditions),
+				tuple([filters["doc_type"]] + values), as_dict=1)
+	else:
+		entries = frappe.db.sql("""
+			SELECT
+				dt.name, dt.customer, dt.territory, dt.%s as posting_date, dt_item.item_code,
+				st.sales_person, st.allocated_percentage, dt_item.warehouse,
+			CASE
+				WHEN dt.status = "Closed" THEN dt_item.%s * dt_item.conversion_factor
+				ELSE dt_item.stock_qty
+			END as stock_qty,
+			CASE
+				WHEN dt.status = "Closed" THEN (dt_item.base_net_rate * dt_item.%s * dt_item.conversion_factor)
+				ELSE dt_item.base_net_amount
+			END as base_net_amount,
+			CASE
+				WHEN dt.status = "Closed" THEN ((dt_item.base_net_rate * dt_item.%s * dt_item.conversion_factor) * st.allocated_percentage/100)
+				ELSE dt_item.base_net_amount * st.allocated_percentage/100
+			END as contribution_amt
+			FROM
+				`tab%s` dt, `tab%s Item` dt_item, `tabSales Team` st
+			WHERE
+				st.parent = dt.name and dt.name = dt_item.parent and st.parenttype = %s
+				and dt.docstatus = 1 %s order by st.sales_person, dt.name desc
+			""" %(date_field, qty_field, qty_field, qty_field, filters["doc_type"], filters["doc_type"], '%s', conditions),
+				tuple([filters["doc_type"]] + values), as_dict=1)
 
 	return entries
 
@@ -177,7 +257,10 @@ def get_conditions(filters, date_field):
 
 	for field in ["company", "customer", "territory"]:
 		if filters.get(field):
-			conditions.append("dt.{0}=%s".format(field))
+			if filters.get("doc_type") == "Payment Entry" and field == "customer":
+				conditions.append("dt.party=%s".format(field))
+			else:
+				conditions.append("dt.{0}=%s".format(field))
 			values.append(filters[field])
 
 	if filters.get("sales_person"):
