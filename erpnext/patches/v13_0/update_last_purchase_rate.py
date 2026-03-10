e230f1d04c81e6fcd780dc61c7d3be0bc4bc54fa
fproldan <franciscoproldan@gmail.com>
2026-03-10 16:43:38 +0000

    fix

--- erpnext/patches/v13_0/update_last_purchase_rate.py
+++ erpnext/patches/v13_0/update_last_purchase_rate.py
@@ -3,9 +3,14 @@ from erpnext.buying.utils import update_last_purchase_rate
 
 
 def execute():
-    invoices = frappe.get_all("Purchase Invoice", {"docstatus": 1}, pluck="name")
-    for invoice in invoices:
-        doc = frappe.get_doc("Purchase Invoice", invoice)
-        update_last_purchase_rate(doc, 1)
-
+    for item in frappe.get_all("Item", pluck="name"):
+        last_purchase_invoice = frappe.db.get_value(
+            "Purchase Invoice Item",
+            {"item_code": item},
+            "parent",
+            order_by="creation desc",
+        )
+        if last_purchase_invoice:
+            doc = frappe.get_doc("Purchase Invoice", last_purchase_invoice)
+            update_last_purchase_rate(doc, 1)
     frappe.db.commit()
