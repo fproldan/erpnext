021c81745f05f06dc9733ffeb027df19bd381a2f
fproldan <franciscoproldan@gmail.com>
2026-03-10 18:13:35 +0000

    fix: patch

--- erpnext/patches/v13_0/update_last_purchase_rate.py
+++ erpnext/patches/v13_0/update_last_purchase_rate.py
@@ -1,16 +1,20 @@
 import frappe
-from erpnext.buying.utils import update_last_purchase_rate
+from frappe.utils import flt
 
 
 def execute():
-    for item in frappe.get_all("Item", pluck="name"):
-        last_purchase_invoice = frappe.db.get_value(
-            "Purchase Invoice Item",
-            {"item_code": item},
-            "parent",
-            order_by="creation desc",
-        )
-        if last_purchase_invoice:
-            doc = frappe.get_doc("Purchase Invoice", last_purchase_invoice)
-            update_last_purchase_rate(doc, 1)
-    frappe.db.commit()
+	for item in frappe.get_all("Item", pluck="name"):
+		last_purchase_invoice = frappe.db.get_value(
+			"Purchase Invoice Item",
+			{"item_code": item},
+			"name",
+			order_by="creation desc",
+		)
+		last_purchase_rate = None
+		if last_purchase_invoice:
+			d = frappe.get_doc("Purchase Invoice Item", last_purchase_invoice)
+			if flt(d.conversion_factor):
+				last_purchase_rate = flt(d.base_net_rate) / flt(d.conversion_factor)
+		
+		frappe.db.set_value('Item', d.item_code, 'last_purchase_rate', flt(last_purchase_rate))                 
+		frappe.db.commit()
